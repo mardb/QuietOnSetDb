331f1709be5e68964310acda2c96f17013d8685f
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-12-03 19:55:05 -0800

    editing readme

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ const ctrl = require('../controllers');
 
 
 router.get('/votes', ctrl.votes.index);
-router.get('/votess/:id', ctrl.votes.show);
+router.get('/votes/:id', ctrl.votes.show);
 router.post('/votes', ctrl.votes.create);
 router.put('/votes/:id', ctrl.votes.update);
 router.delete('votes/:id', ctrl.votes.destroy);
